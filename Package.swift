7ccbec4e9fa99c36f949af128cbf48d88f0e6760
kouki-shito <likegameskou@gmail.com>
2025-10-08 23:51:54 +0900

    fix: delete makefile and rewrite to fastlane

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "PhosphorBinary",
-            url: "https://github.com/kouki-shito/swift-xc-phospor/releases/download/1.0.0/PhosphorBinary-1.0.0.xcframework.zip",
-            checksum: "0f4a21397395ff8276cd74c5145cdcb095006c85b680a8108136e4aef132050d"
+            url: "https://github.com/kouki-shito/swift-xc-phospor/releases/download/1.0/PhosphorBinary.xcframework.zip",
+            checksum: "347d9757c8a07512c440de19378f613cf96c00aff2327406754fdc95e8a2d2df"
         )
     ]
 )
