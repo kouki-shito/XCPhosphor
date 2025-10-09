1fb76ee696d83c030e89af5b3831930b44aeed74
kouki-shito <likegameskou@gmail.com>
2025-10-10 05:04:19 +0900

    fix: change target name

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "XCPhosphor",
-            targets: ["XCPhosphor"]),
+            targets: ["PhosphorBinary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "XCPhosphor",
+            name: "PhosphorBinary",
             url: "https://github.com/kouki-shito/swift-xc-phosphor/releases/download/1.0.1/PhosphorBinary.xcframework.zip",
             checksum: "52810c62557e6036be88b4cbcfbd3e8271367ef992abf95136c8e64a8776cd5b"
         )
