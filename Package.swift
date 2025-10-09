ef06f472e8bce630afc47581d5552976e71dd4f1
kouki-shito <likegameskou@gmail.com>
2025-10-09 22:14:27 +0900

    fix: package version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Phosphor"
+            name: "Phosphor",
+            dependencies: ["PhosphorBinary"]
+        ),
+        .binaryTarget(
+            name: "PhosphorBinary",
+            url: "https://github.com/kouki-shito/swift-xc-phospor/releases/download/1.0.0/PhosphorBinary.xcframework.zip",
+            checksum: "94cef3cddc32de382a96e85a2b226a219fdabfc3eff9c9ba54e94f41591e4cdc"
         )
     ]
 )
